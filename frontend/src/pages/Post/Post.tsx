af6573f2209398bf515e72ef48419d2a8aa8fef4
Emanuel Henrique <emanuelhenriquefs@gmail.com>
2026-01-16 17:56:47 -0300

    put target=_blank in tag a

--- frontend/src/pages/Post/Post.tsx
+++ frontend/src/pages/Post/Post.tsx
@@ -139,7 +139,9 @@ export function Post() {
                   {repository?.homepage ? (
                     <>
                       <LinkIcon size={20} color="#41704e" />
-                      <a href={repository.homepage}>{repository.homepage}</a>
+                      <a href={repository.homepage} target="_blank">
+                        {repository.homepage}
+                      </a>
                     </>
                   ) : null}
                 </span>
